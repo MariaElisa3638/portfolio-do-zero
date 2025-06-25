36639c5fb8fbcf845706673b80977b2934d58a0f
MariaElisa3638 <93793224+MariaElisa3638@users.noreply.github.com>
2025-06-25 13:18:28 -0300

    Update script.js

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -10,3 +10,11 @@ toggleTheme.classList.toggle('bi-sun');
 toggleTheme.classList.toggle('bi-moon-stars');
 }
 toggleTheme.addEventListener('click', changeTheme);
+
+const menuLinks = document.querySelectorAll(".menu__link");
+menuLinks.forEach(item => {
+    item.addEventListener("click", ()=>{
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
